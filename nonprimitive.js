dc16203aaec31782ba578642385e28e855018a6a
MD-Waseem17 <mdwaseem069582@gmail.com>
2025-05-28 07:01:38 +0000

    added code

---
+++ nonprimitive.js
@@ -0,0 +1,25 @@
+// primitive you can store single type of data 
+
+// in non primitive you can store 
+// multyple type of data types 
+
+
+// Example :
+// Object
+// Arrays
+
+// objects can store multiple types of data types 
+// in objects we can store data in key value pair 
+//left side is key
+//right side is value 
+
+let person = {
+    fname: "mdwaseem",
+age:22,
+isAlive: true,
+issleep: null
+}
+
+console.log(person)
+console.log(person.fname)
+console.log(person.age)
---
+++ objact.js
@@ -0,0 +1,27 @@
+//read
+//write
+//update
+//delet
+
+//all these in object
+
+let person = {
+    firstname:"MD",
+    lastname:"MD Waseem",
+    email:"mdwaseem099582@gmail.com"
+}
+
+console.log(person)
+
+//to add a new key in a object
+
+//phone :"99669576856"
+
+person.phone ="9966957686" // add
+console.log(person)
+
+person.age = 24 ; //update 
+console.log(person)
+
+delete person.age 
+console.log(person)
